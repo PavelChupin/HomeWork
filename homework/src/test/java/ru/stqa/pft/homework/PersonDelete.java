841cc3b8830635839c98ef0fd5313c6863c78394
PavelChupin <pavel.chupin@gmail.com>
2017-02-26 23:39:23 +0700

    Отлажана ошибка закрытия модального окна теста удаления контакта

--- homework/src/test/java/ru/stqa/pft/homework/PersonDelete.java
+++ homework/src/test/java/ru/stqa/pft/homework/PersonDelete.java
@@ -36,6 +36,7 @@ public class PersonDelete {
         wd.findElement(By.linkText("home")).click();
         wd.findElement(By.name("selected[]")).click();
         wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+        wd.switchTo().alert().accept();
         wd.findElement(By.linkText("home")).click();
     }
     
