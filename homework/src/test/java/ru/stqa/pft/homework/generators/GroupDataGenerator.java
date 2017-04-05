5d53606cd91ef3e3a5c0fc14f1a1ebb861b96376
PavelChupin <pavel.chupin@gmail.com>
2017-04-05 15:31:23 +0700

    ДЗ №17 починка теста добавления группы в формате файла json были лишние символы \n

--- homework/src/test/java/ru/stqa/pft/homework/generators/GroupDataGenerator.java
+++ homework/src/test/java/ru/stqa/pft/homework/generators/GroupDataGenerator.java
@@ -122,8 +122,8 @@ public class GroupDataGenerator {
             groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i))
                     .withFooter(String.format("footer %s", i)));
                     */
-            groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header\n%s", i))
-                    .withFooter(String.format("footer\n%s", i)));
+            groups.add(new GroupData().withName(String.format("test %s", i)).withHeader(String.format("header %s", i))
+                    .withFooter(String.format("footer %s", i)));
         }
         return groups;
     }
