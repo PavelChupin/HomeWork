e0c7c86d7aaf9197911d06c8a43b4816be4c53f5
PavelChupin <pavel.chupin@gmail.com>
2017-04-19 08:59:58 +0700

    ДЗ №19

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,60 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Summoner on 27.02.2017.
+ */
+public class ApplicationManager {
+    private final Properties properties;
+    WebDriver wd;
+
+    private String browser;
+
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+        properties = new Properties();
+
+    }
+
+    public void init() throws IOException {
+        //Создаем конфигурационный файл
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        if (browser.equals(BrowserType.FIREFOX)) {
+            //Если не находится исполняемый файл браузера то
+            //FirefoxBinary bin = new FirefoxBinary(new File("c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"));
+            //wd = new FirefoxDriver(bin, new FirefoxProfile());
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
+        //wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        //wd.get("http://localhost/addressbook/");
+        //Используем свойства из конфигурационного файла
+        wd.get(properties.getProperty("web.baseUrl"));
+    }
+
+
+    public void stop() {
+        wd.quit();
+    }
+
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("Alexei");
+
+        Square s = new Square(5);
+        System.out.println("??????? ???????? ?? ???????? " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("??????? ?????????????? ?? ????????? " + r.a + " ? " + r.b + " = " + r.area());
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+}
